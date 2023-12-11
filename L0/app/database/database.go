b0caf662b8309315f272a081f86379c17293e089
Dias <diasminibaev@bk.ru>
2023-12-11 21:41:33 +0400

    removing unnecessary functionality

--- L0/app/database/database.go
+++ L0/app/database/database.go
@@ -32,19 +32,6 @@ func New(user, password, dbname, host string, port int) *Database {
 	return &db
 }
 
-// func (database *Database) Open(user, password, dbname, host string, port int) (*sql.DB, error) {
-// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-// 		host, port, user, password, dbname)
-
-// 	db, err := sql.Open(driverName, psqlInfo)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	database.conn = db
-
-//		return db, nil
-//	}
 func (database *Database) Open() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		database.host, database.port, database.user, database.password, database.dbname)
@@ -64,23 +51,5 @@ func (database *Database) Close() {
 		if err := database.conn.Close(); err != nil {
 			log.Println("Error closing database connection:", err)
 		}
-	}
-}
-
-func (database *Database) CreateTableIfExist(db *sql.DB) {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS orders_json (" +
-		"id serial primary key," +
-		"data jsonb" +
-		");")
-
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func (database *Database) Insert(db *sql.DB, data []byte) {
-	_, err := db.Exec("INSERT INTO orders_json (data) VALUES ($1)", data)
-	if err != nil {
-		log.Printf("Error inserting data into PostgreSQL: %v", err)
 	}
 }
