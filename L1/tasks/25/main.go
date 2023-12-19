b974b52206ffaccf6317cde8670275db1fdb96cd
Dias <diasminibaev@bk.ru>
2023-12-19 21:31:08 +0400

    Add Sleep function from Unix and Windows using cgo

--- L1/tasks/25/main.go
+++ L1/tasks/25/main.go
@@ -2,6 +2,10 @@ package main
 
 // Реализовать собственную функцию sleep.
 
+// #include <unistd.h>
+// #include <synchapi.h>
+import "C"
+
 import (
 	"fmt"
 	"time"
@@ -12,11 +16,21 @@ func customSleep(sec int) {
 	<-time.After(time.Duration(sec) * time.Second)
 }
 
+func customSleepUnix(sec uint) {
+	// C.Sleep()
+	C.sleep(C.uint(sec))
+
+}
+
+func customSleepWindows(ms uint) {
+	C.Sleep(C.ulong(ms * 1000))
+}
+
 func main() {
 	fmt.Println("Before sleep")
 
 	// Вызываем собственную функцию "sleep" на 3 секунды
-	customSleep(3)
+	customSleepUnix(10)
 
 	fmt.Println("After sleep")
 }
